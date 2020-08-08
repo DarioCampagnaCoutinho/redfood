3eb0c7afe85ca808191667cb17dcb61d8413e6a7
Dario Campagna <dario@localhost.localdomain>
2020-08-08 18:00:24 -0300

    Criação do projeto

---
+++ src/main/java/com/java/redfood/RedfoodApplication.java
@@ -0,0 +1,13 @@
+package com.java.redfood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedfoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedfoodApplication.class, args);
+	}
+
+}
