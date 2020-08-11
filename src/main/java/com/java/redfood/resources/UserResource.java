9b21c6de58bd22df594d7605962cbb07b34cc866
Dario Campagna <dario@localhost.localdomain>
2020-08-11 09:43:47 -0300

    Configurando a classe TestConfig, UserService e UserResource.

--- src/main/java/com/java/redfood/resources/UserResource.java
+++ src/main/java/com/java/redfood/resources/UserResource.java
@@ -1,18 +1,32 @@
 package com.java.redfood.resources;
 
 import com.java.redfood.domain.User;
+import com.java.redfood.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping
-    public ResponseEntity<User> findAll(){
-        User u = new User(1L, "Dario", "dario@gmaio.com", "33 94567-9078", "1234");
-        return ResponseEntity.ok().body(u);
+    public ResponseEntity<List<User>> findAll(){
+        List<User> list = userService.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable("id") Long id){
+        User user = userService.findById(id);
+        return ResponseEntity.ok().body(user);
     }
 }
