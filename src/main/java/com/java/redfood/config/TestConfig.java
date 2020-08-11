e85316947841c2ca47b6788a43c4b0342d286826
Dario Campagna <dario@localhost.localdomain>
2020-08-11 11:13:35 -0300

    Configurando a classe Order - Parte 2

--- src/main/java/com/java/redfood/config/TestConfig.java
+++ src/main/java/com/java/redfood/config/TestConfig.java
@@ -1,12 +1,15 @@
 package com.java.redfood.config;
 
+import com.java.redfood.domain.Order;
 import com.java.redfood.domain.User;
+import com.java.redfood.repositories.OrderRepository;
 import com.java.redfood.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 @Configuration
@@ -16,11 +19,20 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private OrderRepository orderRepository;
+
     @Override
     public void run(String... args) throws Exception {
 
-        User u1 = new User(1L, "Dario", "dario@gmaio.com", "33 94567-9078", "1234");
-        User u2 = new User(2L, "Mario", "mario@gmaio.com", "34 94567-9090", "1256");
+        User u1 = new User(null, "Dario", "dario@gmaio.com", "33 94567-9078", "1234");
+        User u2 = new User(null, "Mario", "mario@gmaio.com", "34 94567-9090", "1256");
+
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
+        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+
         userRepository.saveAll(Arrays.asList(u1, u2));
+        orderRepository.saveAll(Arrays.asList(o1, o2, o3));
     }
 }
--- src/main/java/com/java/redfood/repositories/OrderRepository.java
+++ src/main/java/com/java/redfood/repositories/OrderRepository.java
@@ -1,9 +1,9 @@
 package com.java.redfood.repositories;
 
-import com.java.redfood.domain.User;
+import com.java.redfood.domain.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface OrderRepository extends JpaRepository<Order, Long> {
 }
