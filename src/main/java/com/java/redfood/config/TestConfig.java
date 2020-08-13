8aa486c5fa5889ce1e23a3d1cc6985ccaa0ebb7c
Dario Campagna <dario@localhost.localdomain>
2020-08-13 09:30:24 -0300

    Order Status enum

--- src/main/java/com/java/redfood/config/TestConfig.java
+++ src/main/java/com/java/redfood/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.java.redfood.config;
 
 import com.java.redfood.domain.Order;
 import com.java.redfood.domain.User;
+import com.java.redfood.domain.enums.OrderStatus;
 import com.java.redfood.repositories.OrderRepository;
 import com.java.redfood.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,9 +29,9 @@ public class TestConfig implements CommandLineRunner {
         User u1 = new User(null, "Dario", "dario@gmaio.com", "33 94567-9078", "1234");
         User u2 = new User(null, "Mario", "mario@gmaio.com", "34 94567-9090", "1256");
 
-        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+        Order o1 = new Order(null, Instant.parse("2020-06-20T19:53:07Z"), OrderStatus.DELIVERED, u1);
+        Order o2 = new Order(null, Instant.parse("2020-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
+        Order o3 = new Order(null, Instant.parse("2020-07-22T15:21:22Z"), OrderStatus.PAID, u1);
 
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
