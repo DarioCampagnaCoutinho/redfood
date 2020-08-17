6ad3c05cb64b35113fd39d73baef3a31639c9812
Dario Campagna <dario@localhost.localdomain>
2020-08-17 13:06:36 -0300

    Tratamento de exceção delete - usuários

---
+++ src/main/java/com/java/redfood/services/exceptions/DataBaseException.java
@@ -0,0 +1,8 @@
+package com.java.redfood.services.exceptions;
+
+public class DataBaseException extends RuntimeException {
+
+    public DataBaseException(String msg){
+        super(msg);
+    }
+}
